040152c4aa39e2b7fbfeb1af8bcd4eca417f512f
lyc123 <980469887@qq.com>
2018-02-26 17:38:13 +0800

    。。。

--- application/api/controller/v1/Index.php
+++ application/api/controller/v1/Index.php
@@ -10,12 +10,28 @@ namespace app\api\controller\v1;
 
 
 use app\api\controller\Common;
+use think\Cache;
 
 class Index extends Common
 {
     public function getIndexData(){
+
+        $cache_model = new Cache();
+        $cache_key = 'cate_list';
+        $cache_expire_time = 1200;
+        $cate_info = $cache_model->get($cache_key);
+        if($cate_info){
+
+            $cates = $cate_info;
+
+        }else{
+
+            $cates = model('Cate')->getCate();
+
+            $cache_model->set($cache_key,$cates,$cache_expire_time);
+        }
         //首页分类
-        $cates = model('Cate')->getCate();
+//        $cates = model('Cate')->getCate();
 
         //首页企业列表
         $companys = model('CompanyList')->getCompanyListForIndex();
